45a5ce57380b8cc32b7370a92c8bc13cbe86cf35
Kaio Garcia <50223968+kaiofbgarcia@users.noreply.github.com>
2022-03-25 16:52:55 -0300

    Add files via upload

---
+++ N-Queen/NQueenProblemBB.java
@@ -0,0 +1,59 @@
+/* Branch and bound */
+
+import java.util.Arrays;
+
+class NQueenProblemBB {
+  private static boolean isSafe(char[][] mat, int r, int c) {
+    for (int i = 0; i < r; i++) {
+      if (mat[i][c] == 'Q') {
+        return false;
+      }
+    }
+    for (int i = r, j = c; i >= 0 && j >= 0; i--, j--) {
+      if (mat[i][j] == 'Q') {
+        return false;
+      }
+    }
+    for (int i = r, j = c; i >= 0 && j < mat.length; i--, j++) {
+      if (mat[i][j] == 'Q') {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private static void printSolution(char[][] mat) {
+    for (char[] chars : mat) {
+      System.out.println(Arrays.toString(chars).replaceAll(",", ""));
+    }
+    System.out.println();
+  }
+
+  private static void nQueen(char[][] mat, int r) {
+    if (r == mat.length) {
+      printSolution(mat);
+      return;
+    }
+    for (int i = 0; i < mat.length; i++) {
+      if (isSafe(mat, r, i)) {
+        mat[r][i] = 'Q';
+        nQueen(mat, r + 1);
+        mat[r][i] = '–';
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    int N = 8;
+    long tempoInicial = System.currentTimeMillis();
+    char[][] mat = new char[N][N];
+    for (int i = 0; i < N; i++) {
+      Arrays.fill(mat[i], '–');
+    }
+    nQueen(mat, 0);
+    System.out.println(System.currentTimeMillis() - tempoInicial);
+  }
+}
+
+// This code is contributed by sujitmeshram
+// Referencia : https://www.geeksforgeeks.org/n-queen-problem-using-branch-and-bound/
---
+++ N-Queen/NQueenProblemBackAll.java
@@ -0,0 +1,45 @@
+/* Backtracking */
+
+import java.util.*;
+
+class GfG {
+    static List<List<Integer>> nQueen(int n) {
+        cols = new boolean[n];
+        leftDiagonal = new boolean[2 * n];
+        rightDiagonal = new boolean[2 * n];
+        result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            temp.add(0);
+        solveNQUtil(result, n, 0, temp);
+        return result;
+    }
+
+    private static void solveNQUtil(List<List<Integer>> result, int n, int row, List<Integer> comb) {
+        if (row == n) {
+            result.add(new ArrayList<>(comb));
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            if (cols[col] || leftDiagonal[row + col] || rightDiagonal[row - col + n])
+                continue;
+            cols[col] = leftDiagonal[row + col] = rightDiagonal[row - col + n] = true;
+            comb.set(col, row + 1);
+            solveNQUtil(result, n, row + 1, comb);
+            cols[col] = leftDiagonal[row + col] = rightDiagonal[row - col + n] = false;
+        }
+    }
+
+    static List<List<Integer>> result = new ArrayList<List<Integer>>();
+    static boolean[] cols, leftDiagonal, rightDiagonal;
+
+    public static void main(String[] args) {
+        int n = 4;
+        long tempoInicial = System.currentTimeMillis();
+        List<List<Integer>> res = nQueen(n);
+        System.out.println(res);
+        System.out.println(System.currentTimeMillis() - tempoInicial);
+    }
+}
+
+// Referencia : https://acervolima.com/imprimindo-todas-as-solucoes-no-problema-n-queen/
